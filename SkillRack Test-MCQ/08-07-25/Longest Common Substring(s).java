4730a598c4810c99b6f9e45dcef0471c09e67baa
ashwinn-si <ashwin23cse@gmail.com>
2025-08-29 11:02:58 +0530

    DONE...!

--- SkillRack Test-MCQ/08-07-25/Longest Common Substring(s).java
+++ SkillRack Test-MCQ/08-07-25/Longest Common Substring(s).java
@@ -16,3 +16,35 @@
  * ven
  * 
  */
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s1 = sc.nextLine();
+    String s2 = sc.nextLine();
+    int R = s1.length();
+    int C = s2.length();
+    int dp[][] = new int[R + 1][C + 1];
+    int maxLength = 0;
+
+    for (int i = 1; i <= R; i++) {
+      for (int j = 1; j <= C; j++) {
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+          maxLength = Math.max(maxLength, dp[i][j]);
+        }
+      }
+    }
+
+    for (int i = 1; i <= R; i++) {
+      for (int j = 1; j <= C; j++) {
+        if (dp[i][j] == maxLength) {
+          System.out.println(s1.substring(i - maxLength, i));
+        }
+      }
+    }
+
+  }
+}
